bee4fbfd5b8be651bac295b69d34c2a742c6ad25
Rebecca Hogewood <rebeccahogewood@gmail.com>
2023-01-20 12:49:36 -0500

    connected HDL_driver function to interface

--- blood_calc.py
+++ blood_calc.py
@@ -2,16 +2,24 @@ def interface():
     print("Blood Calculator")
     while True:
         print("Options:")
+        print("1 - HDL")
         print("9 - Quit")
         choice = input("Select an option: ")
         if choice == "9":
             return
+        elif choice == "1":
+            HDL_driver()
+
+def HDL_driver():
+    HDL_in = HDL_input()
+    HDl_ana = HDL_analysis(HDL_in)
 
 
 def HDL_input():
     HDL_value = input("Enter the HDL result:" )
     HDL_value = int(HDL_value)
     return HDL_value
+
 
 def HDL_analysis(HDL_int):
     if HDL_int >=60:
@@ -20,7 +28,7 @@ def HDL_analysis(HDL_int):
         answer = "Borderline Low"
     else:
         answer = "Low"
-    return answer
+    return answer 
 
 
 
