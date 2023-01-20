e7fe5bb98dc7e99adaa205c8db458212562624a3
Rebecca Hogewood <rebeccahogewood@gmail.com>
2023-01-20 12:31:54 -0500

    Developed basic interface with exit option

---
+++ blood_calc.py
@@ -0,0 +1,10 @@
+def interface():
+    print("Blood Calculator")
+    while True:
+        print("Options:")
+        print("9 - Quit")
+        choice = input("Select an option: ")
+        if choice == "9":
+            return
+
+interface()
