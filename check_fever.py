825e78fe505ba1d12ef73ecfceb4a9f79ec9f003
Rebecca Hogewood <rebeccahogewood@gmail.com>
2023-02-03 13:17:59 -0500

    example of writing unit test before code

---
+++ check_fever.py
@@ -0,0 +1,12 @@
+def test_check_fever():
+	from blood_calc import tech_check_fever
+	input_temps = [97, 98.6, 100.1, 103, 98.4]
+	answer = check_fever(input_temps)
+	expected = True
+	asser answer == expected
+
+
+def check_fever(input_list):
+	for temperature in input_list:
+	if temperature > 100.5:
+		return True
