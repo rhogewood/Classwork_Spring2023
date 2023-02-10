922a30487e9b4e5e9a3207e191d55c7dbd2a4845
Rebecca Hogewood <rebeccahogewood@gmail.com>
2023-02-10 10:37:53 -0500

    modified unit testing to fit PEP8 standard

--- test_line_coord.py
+++ test_line_coord.py
@@ -1,9 +1,9 @@
 import pytest
 
-def test_find_line():
-	from line_coord import find_line
-	input = [3,4,5,6,7]
-	answer = find_line(input)
-	expected = 8
-	assert answer == expected
 
+def test_find_line():
+    from line_coord import find_line
+    input = [3, 4, 5, 6, 7]
+    answer = find_line(input)
+    expected = 8
+    assert answer == expected
