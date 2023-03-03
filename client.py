b72bf5ef32e882de827e1ed57e87897ab89d8e53
Rebecca Hogewood <rebeccahogewood@gmail.com>
2023-03-03 13:15:28 -0500

    fixed pep8

--- client.py
+++ client.py
@@ -6,17 +6,14 @@ r = requests.get(server + "/info")
 print(r.status_code)
 print(r.text)
 
-
 out_data = {"name": "Rebecca Hogewood", "HDL_value": 65}
 r = requests.post(server + "/HDL_analysis", json=out_data)
 print(r.status_code)
 print(r.text)
 
-
 r = requests.get(server + "/add_two/5/26")
 print(r.status_code)
 print(r.json())
-
 
 out_data = {"a": 2, "b": -3}
 r = requests.post(server + "/add", json=out_data)
@@ -24,5 +21,4 @@ print(r.status_code)
 print(r.text)
 if r.status_code == 200:
     x = r.json()
-    print(x+2)
-
+    print(x + 2)
